8f95bfaee4d087faf51f7ed7be192054a7040891
Prajitsingh1105 <prajitsingh05@gmail.com>
2026-05-31 19:12:08 +0530

    optimised diameter

--- Trees/06diameterOfTree.cpp
+++ Trees/06diameterOfTree.cpp
@@ -54,9 +54,30 @@ int diameter(node* root){
     return ans;
 }
 
+//optimised
+
+pair<int,int> diameterOptimised(node* root){
+    if(root == NULL){
+        pair<int,int> p = make_pair(0,0);
+        return p;
+    }
+    pair<int,int> left = diameterOptimised(root->left);
+    pair<int,int> right = diameterOptimised(root->right);
+    int op1 = left.first;
+    int op2 = right.first;
+    int op3 = left.second + right.second  +1;
+
+    pair<int,int> ans;
+    ans.first = max(op1,max(op2,op3));
+    ans.second = max(op1,op2) + 1;
+    return ans;
+}
+
 int main(){
     node* root = NULL;
     root = buildTree(root);
-    cout<<"Diameter of tree: "<<diameter(root);
+    pair<int,int> diameter;
+    diameter.first = diameterOptimised(root).first;
+    cout<<"Diameter of tree: "<<diameter.first;
     return 0;
 }
