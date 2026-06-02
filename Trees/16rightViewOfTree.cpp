29c21c603cc3138a69ecbe2319e182fdf107188a
Prajitsingh1105 <prajitsingh05@gmail.com>
2026-06-02 22:00:06 +0530

    16 right view of tree

---
+++ Trees/16rightViewOfTree.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+node* buildTree(node* root){
+    int data;
+    cout<<"Enter the data(-1 if null): ";
+    cin>>data;
+    if(data == -1){
+        return NULL;
+    }
+    root = new node(data);
+    cout<<"Enter the data left to "<<data<<endl;
+    root->left = buildTree(root->left);
+    cout<<"Enter the data right to "<<data<<endl;
+    root->right = buildTree(root->right);
+    return root;
+}
+
+void solve(node* root,vector<int> &ans,int level){
+    if(root==NULL) return;
+    if(level==ans.size()) ans.push_back(root->data);
+    solve(root->right,ans,level+1);
+    solve(root->left,ans,level+1);
+}
+
+vector<int> rightView(node* root){
+    vector<int> ans;
+    solve(root,ans,0);
+    return ans;
+}
+
+int main(){
+    node* root = NULL;
+    root = buildTree(root);
+    cout<<"Right-View : "<<endl;
+    vector<int> res = rightView(root);
+    for(int i=0;i<res.size();i++){
+        cout<<res[i]<<" ";
+    }
+    return 0;
+}
