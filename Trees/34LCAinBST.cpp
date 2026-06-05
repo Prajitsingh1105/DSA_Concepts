5deadf61d838ac83974cbae883833340bace1175
Prajitsingh1105 <prajitsingh05@gmail.com>
2026-06-05 22:26:43 +0530

    34 LCA in BST

---
+++ Trees/34LCAinBST.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+node* insertToBST(node* root,int d){
+    if(root==NULL){
+        root = new node(d);
+        return root;
+    }
+    if(d>root->data){
+        root->right=insertToBST(root->right,d);
+    }
+    else{
+        root->left = insertToBST(root->left,d);
+    }
+    return root;
+}
+
+node* takeInput(node* &root){
+    int data;
+    cout<<"Enter the node: ";
+    cin>>data;
+    while(data!=-1){
+        root = insertToBST(root,data);
+        cin>>data;
+    }
+    return root;
+}
+
+void preOrder(node* root){
+    if(root == NULL) return;
+    cout<<root->data<<" ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+int LCAinBST(node* root,int a,int b){
+    if(root==NULL) return -1;
+    if(root->data>a && root->data>b) return LCAinBST(root->left,a,b);
+    if(root->data<a && root->data<b) return LCAinBST(root->right,a,b);
+    return root->data;
+}
+
+int main(){
+    node* root = NULL;
+    root = takeInput(root);
+    preOrder(root);
+    cout<<endl;
+    cout<<"LCA of 40 and 50 are: "<<LCAinBST(root,40,50)<<endl;
+    return 0;
+}
