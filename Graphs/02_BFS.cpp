4fc53c43de49e84c092382259f5020177b1a7ed7
Prajitsingh1105 <prajitsingh05@gmail.com>
2026-06-14 13:49:37 +0530

    02 BFS

---
+++ Graphs/02_BFS.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<unordered_map>
+#include<set>
+#include<vector>
+#include<queue>
+using namespace std;
+
+void prepareAdjList(unordered_map<int,set<int>> &adjList,vector<pair<int,int>> edges){
+    for(int i=0;i<edges.size();i++){
+        int u = edges[i].first;
+        int v = edges[i].second;
+        adjList[u].insert(v);
+        adjList[v].insert(u);
+    }
+}
+
+void bfs(unordered_map<int,set<int>> &adjList,unordered_map<int,bool> &visited,vector<int> &ans,int node){
+    queue<int> q;
+    q.push(node);
+    visited[node] = 1;
+    while(!q.empty()){
+        int frontNode = q.front();
+        q.pop();
+        ans.push_back(frontNode);
+        for(auto i : adjList[frontNode]){
+            if(!visited[i]){
+                q.push(i);
+                visited[i] = 1;
+            }
+        }
+    }
+}
+
+vector<int> BFS(int vertex,vector<pair<int,int>> edges){
+    unordered_map<int,set<int>> adjList;
+    vector<int> ans;
+    unordered_map<int,bool> visited;
+    prepareAdjList(adjList,edges);
+    for(int i=0;i<vertex;i++){
+        if(!visited[i]){
+            bfs(adjList,visited,ans,i);
+        }
+    }
+    return ans;
+}
+
+int main(){
+    vector<pair<int,int>> edges = {
+        {0,3},
+        {3,1},
+        {1,2},
+        {1,4}
+    };
+    int vertex = 1;
+    vector<int> bfsOutput = BFS(vertex,edges);
+    cout<<"BFS traversal of the graph is: "<<endl;
+    for(int i=0;i<bfsOutput.size();i++){
+        cout<<bfsOutput[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
