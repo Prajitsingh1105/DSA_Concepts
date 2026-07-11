7ea6e95b392d329b51b8d4d90eedd7e224f7efba
Prajitsingh1105 <prajitsingh05@gmail.com>
2026-07-11 23:04:39 +0530

    02 fibonacci number

--- Recursion/02_fibo_number.cpp
+++ Recursion/02_fibo_number.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 using namespace std;
 
-class Solution {
-public:
-    int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1)+fib(n-2);
-    }
-};
+int fib(int n) {
+    if(n==0) return 0;
+    if(n==1) return 1;
+    return fib(n-1)+fib(n-2);
+}
 
 int main(){
-    
+    int n;
+    cin>>n;
+    int ans = fib(n);
+    cout<<"Ans: "<<ans<<endl;
 }
